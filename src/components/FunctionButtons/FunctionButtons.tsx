f22f7ef85d03fe0708f222feea06302334b93053
Drew <dcm752052@gmail.com>
2022-11-15 18:52:26 -0500

    Added words undo, erase, and pencil under buttons

--- src/components/FunctionButtons/FunctionButtons.tsx
+++ src/components/FunctionButtons/FunctionButtons.tsx
@@ -4,14 +4,25 @@ import "./functionButtons.css"
 const FunctionButtons: React.FC = () => {
   return (
     <div className="function-buttons">
-      <div className="fn-btn undo">
-        <img src="undo.svg" alt="undo"></img>
+      <div className="fn-btn-wrapper">
+        <div className="fn-btn">
+          <img src="undo.svg" alt="undo"></img>
+        </div>
+        Undo
       </div>
-      <div className="fn-btn erase">
-        <img src="eraser.svg" alt="eraser" />
+      
+      <div className="fn-btn-wrapper">
+        <div className="fn-btn">
+          <img src="eraser.svg" alt="eraser" />
+        </div>
+        Erase
       </div>
-      <div className="fn-btn notes">
-        <img src="lead-pencil.svg" alt="pencil" />
+      
+      <div className="fn-btn-wrapper">
+        <div className="fn-btn">
+          <img src="lead-pencil.svg" alt="pencil" />
+        </div>
+        Pencil
       </div>
     </div>
   )
