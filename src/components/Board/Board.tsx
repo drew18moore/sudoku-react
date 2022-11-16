fc66b1a1c1f67b657ca45b1903e2664e9b5bd952
Drew <dcm752052@gmail.com>
2022-11-15 19:48:58 -0500

    Fixed issue with board and keypad divs not having key properties.

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -11,7 +11,7 @@ const Board: React.FC<BoardProps> = (props) => {
     let horizontalBorder = i === 2 || i === 5 ? "bottom-border" : i === 3 || i === 6 ? "top-border" : ""
     return row.map((val, j) => {
       let verticalBorder = j === 2 || j === 5 ? "right-border" : j === 3 || j === 6 ? "left-border" : ""
-      return <div onClick={() => props.handleClick(i, j, val)} className={`tile ${verticalBorder} ${horizontalBorder}`}>{val === 0 ? "" : val}</div>
+      return <div onClick={() => props.handleClick(i, j, val)} className={`tile ${verticalBorder} ${horizontalBorder}`} key={`${i}-${j}`}>{val === 0 ? "" : val}</div>
     })
   });
 
--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -3,7 +3,7 @@ import "./keypad.css"
 
 const Keypad: React.FC = () => {
   let keys = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => {
-    return <div className="key">{num}</div>
+    return <div className="key" key={num}>{num}</div>
   })
   return (
     <div className="keypad">
